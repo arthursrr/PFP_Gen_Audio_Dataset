a532b258c6547035c10459bbe66c2e021f86ae9f
arthursrr <arthur.serra10@gmail.com>
2021-06-02 16:39:04 -0300

    Modulo de pré-inicialização do projeto.

---
+++ preload.js
@@ -0,0 +1,13 @@
+// Todas as APIs do Node.js estão disponíveis no processo de preload.
+// Tem a mesma sandbox como uma extencao do Chrome.
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
+    }
+  
+    for (const dependency of ['chrome', 'node', 'electron']) {
+      replaceText(`${dependency}-version`, process.versions[dependency])
+    }
+  })
+  
